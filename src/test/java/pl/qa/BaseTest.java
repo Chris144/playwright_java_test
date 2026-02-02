d6f3e34a7f75d2f87c7923c597c1090b9f706b0e
Chris144 <krzysztofktly@gmail.com>
2026-02-02 15:59:22 +0100

    Add GitLab CI pipeline

--- src/test/java/pl/qa/BaseTest.java
+++ src/test/java/pl/qa/BaseTest.java
@@ -21,7 +21,7 @@ public class BaseTest {
     public void setup() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(true)
         );
         page = browser.newPage();
     }
